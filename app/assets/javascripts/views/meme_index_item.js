c7c94593557283d5bc317fbeb030bd583d879f32
Ben <benjamin.c.henderson@gmail.com>
2014-11-28 13:37:04 -0800

    updates index view to use images

--- app/assets/javascripts/views/meme_index_item.js
+++ app/assets/javascripts/views/meme_index_item.js
@@ -5,18 +5,11 @@ App.Views.MemeIndexItem = Backbone.CompositeView.extend({
 	template: JST['memes/index_item'],
 	className: "meme-index-item row",
 	initialize: function(){
-		this.addMemeView();
 	},
 	render: function(){
-		var content = this.template();
+		var content = this.template({ meme: this.model });
 		this.$el.html(content);
-		this.addMemeView();
 		return this;
-	},
-	addMemeView: function(){
-		this.memeView && this.memeView.remove();
-		this.memeView = new App.Views.MemeShow({ model: this.model });
-		this.$(".meme-container").html( this.memeView.render().$el );
 	},
 	beginEdits: function(){
 		Backbone.history.navigate("/memes/" + this.model.id + "/edit", { trigger: true });
