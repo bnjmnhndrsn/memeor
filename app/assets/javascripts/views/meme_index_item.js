612f5aae7d66e67967cbe3939415afc356a86d76
Ben <benjamin.c.henderson@gmail.com>
2014-11-28 16:14:21 -0800

    updates styling and adds buttons to index view

--- app/assets/javascripts/views/meme_index_item.js
+++ app/assets/javascripts/views/meme_index_item.js
@@ -1,9 +1,10 @@
 App.Views.MemeIndexItem = Backbone.CompositeView.extend({
 	events: {
-		"click" : "beginEdits"
+		"click .edit" : "beginEdits",
+		"click .fork" : "beginEdits"
 	},
 	template: JST['memes/index_item'],
-	className: "meme-index-item row",
+	className: "meme-index-item col-md-4 col-xs-6",
 	initialize: function(){
 	},
 	render: function(){
--- app/views/memes/_meme.json.jbuilder
+++ app/views/memes/_meme.json.jbuilder
@@ -1,7 +1,8 @@
-json.extract!(meme, :id, :title, :cached_image)
+json.extract!(meme, :id, :title, :cached_image, :created_at, :updated_at)
 json.image do 
 	json.partial! 'images/image', image: meme.image
 end
 json.captions do
 	json.array! meme.captions, partial: 'captions/caption', as: :caption
 end
+json.time_words time_ago_in_words(meme.created_at)
