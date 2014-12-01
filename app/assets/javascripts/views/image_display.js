15ac9a00ec285ddd10987eee99b8b1706f6b003e
Ben <benjamin.c.henderson@gmail.com>
2014-12-01 14:22:29 -0800

    add index view to images

--- app/assets/javascripts/views/image_display.js
+++ app/assets/javascripts/views/image_display.js
@@ -6,10 +6,12 @@ App.Views.ImageDisplay = Backbone.View.extend({
 	className: "image-display container",
 	initialize: function(){
 		this.listenTo(this.model, "sync change", this.render);
+		this.memesIndex = new App.Views.MemesIndex({ collection: this.collection });
 	},
 	render: function(){
 		var content = this.template({ image: this.model });
 		this.$el.html(content);
+		this.$el.append( this.memesIndex.render().$el );
 		return this;
 	},
 	new: function(){
