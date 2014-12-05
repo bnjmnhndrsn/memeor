048049c2b6bedc5595256136ba61290205163f2e
Ben <benjamin.c.henderson@gmail.com>
2014-12-04 20:57:18 -0800

    removes old view files

--- app/assets/javascripts/views/caption_form.js
+++
@@ -1,5 +0,0 @@
-App.Views.CaptionForm = Backbone.StylableForm.extend({
-	className: "caption-form",
-	template: JST["captions/form"],
-	modelName: "caption"
-});
--- app/assets/javascripts/views/meme_form.js
+++
@@ -1,4 +0,0 @@
-App.Views.MemeForm = Backbone.StylableForm.extend({
-	template: JST["memes/form"],
-	modelName: "meme"
-});
