b343f8f9950c1f5c2669d87b398e55c14cdc20ad
Ben <benjamin.c.henderson@gmail.com>
2014-12-05 14:09:59 -0800

    fixes typo

--- app/assets/javascripts/views/memes_index.js
+++ app/assets/javascripts/views/memes_index.js
@@ -6,11 +6,11 @@ App.Views.MemesIndex = Backbone.CompositeView.extend({
 		this.collection.each(function(meme){
 			this.addMemeView(meme);
 		}.bind(this));
-		
+	
 		this.listenTo(this.collection, "add", this.addMemeView);
 	},
 	render: function(){
-		var rendered = this.template();
+		var rendered = this.template({ loaded: this.loaded });
 		this.$el.html(rendered);
 		this.attachSubviews();
 		return this;
@@ -18,5 +18,9 @@ App.Views.MemesIndex = Backbone.CompositeView.extend({
 	addMemeView: function(meme){
 		var view = new App.Views.MemeIndexItem({ model: meme });
 		this.addSubview(".memes", view);
+	},
+	endLoading: function(){
+		this.loaded = true;
+		this.render();
 	}
 });
