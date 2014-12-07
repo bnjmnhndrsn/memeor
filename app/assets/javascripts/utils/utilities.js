32c4a245ff6c9d35c3dd8d389d1a6587d1f0b6e9
Ben <benjamin.c.henderson@gmail.com>
2014-12-06 22:29:58 -0800

    adds responsive bootstrap toolkit

--- app/assets/javascripts/utils/utilities.js
+++ app/assets/javascripts/utils/utilities.js
@@ -1,6 +1,6 @@
 App.Utils.setBGImage = function(url){
 	var val = url ? "url('" + url + "')" : "";
-	if (val) {
+	if (val && !ResponsiveBootstrapToolkit.is('xs')) {
 		var image = new Image();
 		var timestamp = App.Utils.BGImageTimestamp = new Date();
 		image.onload = function(){
