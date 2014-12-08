828c5e1cbab7aab3c39532e8e055b6cb5eb7aa2c
Ben <benjamin.c.henderson@gmail.com>
2014-12-08 10:56:36 -0800

    adds window resize re-rerendering

--- app/assets/javascripts/views/image_index_item.js
+++ app/assets/javascripts/views/image_index_item.js
@@ -5,6 +5,8 @@ App.Views.ImageIndexItem = Backbone.CompositeView.extend({
 	template: JST['images/index_item'],
 	className: "image-index-item text-center col-md-3 col-sm-4 col-xs-12",
 	initialize: function(){
+		this.resizeHandler = this.render.bind(this);
+		$(window).on("viewportResize", this.resizeHandler);
 		this.listenTo(this.model, "change", this.render );
 	},
 	render: function(){
@@ -13,6 +15,7 @@ App.Views.ImageIndexItem = Backbone.CompositeView.extend({
 		return this;
 	},
 	changeBGImage: function(){
+        $(window).off("viewportResize", this.resizeHandler);
 		App.Utils.setBGImage(this.model.get("image_src_full"));
 	}
 });
