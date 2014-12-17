6f678e3dd1e73b48c64da565df724e069b7c07b4
Ben <benjamin.c.henderson@gmail.com>
2014-12-16 22:45:39 -0800

    adds about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'static_pages#root'
+  get 'about', to: "static_pages#about"
   
   resources :memes, only: [:index, :show, :create, :update, :destroy], :defaults => { :format => 'json' }
   resources :captions, only: [:create, :update, :destroy], :defaults => { :format => 'json' }
